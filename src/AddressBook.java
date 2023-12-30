482c6658cf27591b802f90a925cdcd43e853a2f0
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2023-12-30 20:51:28 +0530

    Address book Problem

---
+++ src/AddressBook.java
@@ -0,0 +1,8 @@
+public class AddressBook {
+    /* Start with Displaying Welcome to Address Book Program in
+AddressBookMain class on START Master Branch*/
+    public static  void main(String[] args)
+    {
+        System.out.println("Welcome to Address Book Problem");
+    }
+}
