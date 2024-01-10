8b5c75100a042a1c43a5c09545834a3a5bd88b37
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2024-01-10 11:45:50 +0530

    Delete contact by firstname

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -5,14 +5,16 @@ import java.util.Scanner;
 public class AddressBook {
     //Create a arraylist of Contact class
     List<Contact> contactlist = new ArrayList<>();
-   //enter a proper choice
-    void enterProperChoice ()
-    {
+
+    //enter a proper choice
+    void enterProperChoice() {
         System.out.println("1.Create new Contact");
         System.out.println("2.Display contact");
+        System.out.println("3.Edit contact");
+        System.out.println("4.Delete contact");
     }
 
-   //Create contact method
+    //Create contact method
     public Contact createUserInput() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the FirstName");
@@ -35,12 +37,13 @@ public class AddressBook {
         Contact contact = new Contact(firstname, lastname, address, city, state, zip, phoneNumber, email);
         return contact;
     }
+
     //add contact from a list
-    void addContact(Contact contact)
-    {
+    void addContact(Contact contact) {
         contactlist.add(contact);
     }
- //Display contact using foreach loop in display method.
+
+    //Display contact using foreach loop in display method.
     public void displayContact() {
         for (Contact contact : contactlist) {
             System.out.println(contact.getFirstName() + " " + contact.getLastName() + " " + contact.getAddress() + " "
@@ -50,30 +53,52 @@ public class AddressBook {
         }
 
     }
-    public static void main (String[]args){
-            AddressBook addressBook = new AddressBook();
-            System.out.println("Welcome to Address Book Problem");
-            Scanner sc = new Scanner(System.in);
-            int option;
-            do {
-                addressBook.enterProperChoice();
-                option = sc.nextInt();
-                switch (option) {
-                    case 1:
-                       //Create new contact
-                    Contact contact=addressBook.createUserInput();
-                    addressBook.addContact(contact);
-                        break;
 
-                    case 2:
-                        addressBook.displayContact();
-                        break;
-                    default:
-                        System.out.println("Wrong option");
-                }
+   //Remove contact
+    public void removeContactByfirstname(String firstname)
+    {
+        for(Contact contact:contactlist)
+        {
+            if(contact.getFirstName()==firstname)
+            {
+                contactlist.remove(contact);
             }
-            while (option != 3);
         }
+        System.out.println("Contact Deleted Successfully");
     }
+
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+
+        System.out.println("Welcome to Address Book Problem");
+        Scanner sc = new Scanner(System.in);
+        int option;
+        do {
+            addressBook.enterProperChoice();
+            option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    //Create new contact
+                    Contact con = addressBook.createUserInput();
+                    addressBook.addContact(con);
+                    break;
+
+                case 2:
+                    addressBook.displayContact();
+                    break;
+                case 3:
+                    // addressBook.editContact();
+                    // addressBook.editContactByLastname("Chau");
+                    break;
+                case 4:
+                    addressBook.removeContactByfirstname("Ruchita");
+                    default:
+                    System.out.println("Wrong option");
+            }
+        }
+        while (option != 3);
+    }
+}
+
 
 
